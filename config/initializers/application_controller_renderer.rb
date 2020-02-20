a9c5aa7b94bc03ba73973cd2b9bb8e5a71a2ff8b
田中良征 <tanakaryousei@tanakayoshimasanoMacBook-Pro.local>
2020-02-20 18:19:58 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'ff3cb3a2e7323b1ad586f2162780411771200ec9aef2643f24bc9b65ce38e6db567dcbcfe5b4134ef0f396584599709064014a49954f34b98b88b569502b0af7'

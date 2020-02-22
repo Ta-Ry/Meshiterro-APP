e78641507d675f3ad638eea6e543351188b61f8a
田中良征 <tanakaryousei@tanakayoshimasanoMacBook-Pro.local>
2020-02-22 15:14:00 +0900

    [finish]5-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
 
 #5-6章の始まり
   def edit
-  	
+  	@user = User.find(params[:id])
   end
+
+  def update
+  	user = User.find(params[:id])
+  	user.update(user_params)
+  	redirect_to user_path(user.id)
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
